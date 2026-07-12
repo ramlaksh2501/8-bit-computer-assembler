48cd313839e53e2040503348c07c901cce0c0283
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-07-12 21:40:51 +0530

    INPUT:get the file name :read it till EOF
    facing error while reading idk why need to verify
    
    also need to create an another file for the asm to be stored or the hex (may be both that would be nice )

--- assembler.c
+++ assembler.c
@@ -1,7 +1,30 @@
 #include<stdio.h>
-
-
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<assert.h>
 int main(int argv,char **argc){
-	printf("%s\n",argc[1]);
+	int fd=open(argc[1],O_RDONLY);
+	if(fd==-1){
+		printf("Not a valid file\n");
+		return 3;
+	}
+	char a;
+	if(read(fd,&a,1)){
+		printf("failed to read(may be the file is empty) \n"); 
+		return 2;
+	}
+
+	while(1){
+		assert(read(fd,&a,1));
+		printf("%c",a);
+	}
+	printf("\n");
+
+
+
+
+
+
 	return 0;}
 
