1f254aa45ad3e7e75bfe5e7ec2c1467e5ce14c73
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-07-13 21:15:17 +0530

    READ:read completed:next the parsing
    also need to add logic to check if the input is a .asm/.s file and the no of input (argument)
    
    that's all

--- assembler.c
+++ assembler.c
@@ -3,28 +3,31 @@
 #include<fcntl.h>
 #include<unistd.h>
 #include<assert.h>
+#include<sys/stat.h>
 int main(int argv,char **argc){
 	int fd=open(argc[1],O_RDONLY);
 	if(fd==-1){
 		printf("Not a valid file\n");
-		return 3;
-	}
-	char a;
-	if(read(fd,&a,1)){
-		printf("failed to read(may be the file is empty) \n"); 
 		return 2;
 	}
-
-	while(1){
-		assert(read(fd,&a,1));
-		printf("%c",a);
+	char a;
+	int bufsize;
+	struct stat file_status;
+	if(stat(argc[1],&file_status)==-1){
+			perror("error reading file\n");
+			}
+	bufsize=file_status.st_size+1;
+	char *content=(char *)malloc(sizeof(char)*bufsize);
+	if(content==NULL){
+		perror("memory issue \n");
+		return 3;
+		}
+	if(read(fd,content,bufsize)<=0){
+		perror("failed to read file\n");
+		return 4;
 	}
-	printf("\n");
 
-
-
-
-
+	printf("%s",content);
 
 	return 0;}
 
